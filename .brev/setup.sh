2a6d410dbc3aa0a669c9d39ccf8357d4e397167f
nader <nader@brev.dev>
2021-10-29 23:30:46 +0000

    .brev setup script finally working

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -3,14 +3,23 @@
 ##### Python + Pip + Poetry #####
 sudo apt-get install -y build-essential
 sudo apt install python-is-python3
-# (echo ""; echo "##### Python + Pip + Poetry #####"; echo "";) >> ./.brev/logs/setup.log
 sudo apt-get install -y python3-distutils
 sudo apt-get install -y python3-apt
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 rm get-pip.py
-export PATH=/home/brev/.local/bin:$PATH
+
+export PATH=/home/brev/.local/bin:$PATH 
+# below 4 lines are to get above line behavior
+
+echo "" >> ~/.zshrc
+echo "export PATH=/home/brev/.local/bin:$PATH" >> ~/.zshrc
+echo "" >> ~/.bashrc
+echo "export PATH=/home/brev/.local/bin:$PATH" >> ~/.bashrc
+source ~/.zshrc
+source ~/.bashrc
+
 source $HOME/.poetry/env
 
 ##### mkdocs #####
